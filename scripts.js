c292fba61d10099107abf16ab36f68119f464620
Griffin <griffin@Griffins-MacBook-Air.local>
2017-12-05 09:29:10 -0600

    12-5 exercise, not sure if i did exactly what was expexpected

--- scripts.js
+++ scripts.js
@@ -1,27 +1,54 @@
-let employees = [
-  {
-    name: 'Bob' ,
-    department: 'sales',
-    title: 'sales manager'
-  },
-  {
-    name: 'Tina',
-    department: 'finance',
-    title: 'director of finance'
-  },
-  {
-    name: 'Randy',
-    department: 'IT',
-    title: 'hardware guy'
-  },
-  {
-    name: 'Glenda',
-    department: 'C-suite',
-    title: 'CEO'
+let btn = document.getElementById('btn');
+// let employees = [
+//   { name: 'Bob' , department: 'sales', title: 'sales manager'},
+//   { name: 'Tina', department: 'finance',title: 'director of finance'},
+//   { name: 'Randy', department: 'IT', title: 'hardware guy'},
+//   { name: 'Glenda', department: 'C-suite', title: 'CEO'},
+//   { name: 'Banana', department: ' fruit studies', title: 'peeler'}
+// ];
+
+// // // for (i = 0; i <employees.length; i++){
+// //   console.log(`${employees[i].name}'s job title is ${employees[i].title}`);
+// // }
+
+// // document.getElementById('btn').addEventListener('click', e => {
+// //   console.log('you clicked the button', e.target);
+// // });
+// // document.getElementById('btn').addEventListener('click', e => "hello");
+
+// // let name = 'name';
+// // let job = () => 'Names job';
+// // let number = 2;
+
+// // let col = {
+// //   name, 
+// //   job, 
+// //   number
+// // };
+
+
+let obj = {
+  prop1: 'one',
+  prop2: 2,
+  propMethod: function propToString(){
+    prop1.toString
   }
-];
-
-
-for (i = 0; i <employees.length; i++){
-  console.log(`${employees[i].name}'s job title is ${employees[i].title}`);
 }
+
+let obj2 = Object.create(obj);
+obj2.tProp1 = 1;
+obj2.tProp2 = 'two';
+
+
+  console.log(obj2.hasOwnProperty('prop1'), 'prop1');
+  //false
+  console.log(obj2.hasOwnProperty('propMethod'), 'propMethod');
+  //false
+    console.log(obj2.hasOwnProperty('tProp1'), 'tProp1');
+  //true
+  console.log(obj.hasOwnProperty('tProp1'), 'tProp1');
+  //false
+  obj2.__proto__.hasOwnProperty('prop1')
+  //true
+  
+  
