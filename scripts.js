127b0340bd968478f9165c149dbffdb3b6e336db
Griffin <griffin@Griffins-MacBook-Air.local>
2017-12-06 09:19:48 -0600

    12-6 return string array

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let btn = document.getElementById('btn');
+// let btn = document.getElementById('btn');
 // let employees = [
 //   { name: 'Bob' , department: 'sales', title: 'sales manager'},
 //   { name: 'Tina', department: 'finance',title: 'director of finance'},
@@ -22,33 +22,45 @@ let btn = document.getElementById('btn');
 
 // // let col = {
 // //   name, 
-// //   job, 
-// //   number
-// // };
+// // //   job, 
+// // //   number
+// // // };
 
 
-let obj = {
-  prop1: 'one',
-  prop2: 2,
-  propMethod: function propToString(){
-    prop1.toString
-  }
-}
+// let obj = {
+//   prop1: 'one',
+//   prop2: 2,
+//   propMethod: function propToString(){
+//     prop1.toString
+//   }
+// }
 
-let obj2 = Object.create(obj);
-obj2.tProp1 = 1;
-obj2.tProp2 = 'two';
+// let obj2 = Object.create(obj);
+// obj2.tProp1 = 1;
+// obj2.tProp2 = 'two';
 
 
-  console.log(obj2.hasOwnProperty('prop1'), 'prop1');
-  //false
-  console.log(obj2.hasOwnProperty('propMethod'), 'propMethod');
-  //false
-    console.log(obj2.hasOwnProperty('tProp1'), 'tProp1');
-  //true
-  console.log(obj.hasOwnProperty('tProp1'), 'tProp1');
-  //false
-  obj2.__proto__.hasOwnProperty('prop1')
-  //true
+//   console.log(obj2.hasOwnProperty('prop1'), 'prop1');
+//   //false
+//   console.log(obj2.hasOwnProperty('propMethod'), 'propMethod');
+//   //false
+//     console.log(obj2.hasOwnProperty('tProp1'), 'tProp1');
+//   //true
+//   console.log(obj.hasOwnProperty('tProp1'), 'tProp1');
+//   //false
+//   obj2.__proto__.hasOwnProperty('prop1')
+//   //true
   
   
+let strArr = [45, 'I', true, null, 'am', 3.56, 'a', undefined,{ catchphrase: 'Oh hai, Mark'}, 'JS', 'rockstar']  
+
+
+
+
+  let result = strArr.filter(function(string){
+
+   return typeof(string) === 'string';
+
+  });
+  
+  console.log(result);
